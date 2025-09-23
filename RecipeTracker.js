bbf9819e19b63ffbcd79ad4767e75282f6d79d5f
XAIMOH001 <xaimohchau@gmail.com>
2025-09-23 14:08:04 +0300

    created 2 recipe objects with diffrent properties

---
+++ RecipeTracker.js
@@ -0,0 +1,20 @@
+let recipes = [];
+let recipe1 = {
+    name: "Spaghetti Carbonara",
+    ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+    ratings: [4, 5, 4, 5],
+    cookingTime: 22,
+    totalIngredients: null,
+    difficultyLevel: "",
+    averageRating: null,
+}
+
+const recipe2 = {
+  name: "Chicken Curry",
+  ingredients: ["chicken breast", "coconut milk", "curry powder", "onion", "garlic"],
+  ratings: [4, 5, 5, 5],
+  cookingTime: 42,
+  totalIngredients: null,
+  difficultyLevel: "",
+  averageRating: null,
+}
