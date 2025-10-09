147ca223d7eb52bb3f2fe69f8b0f1a9818e31228
XAIMOH001 <xaimohchau@gmail.com>
2025-10-10 01:04:48 +0300

    added a function that removes all falsy values from an array.

---
+++ FalsyRemover.js
@@ -0,0 +1,10 @@
+function bouncer(arr){
+    let result = [ ];
+    for (let i = 0; i < arr.length; i++){
+        if(Boolean(arr[i])){
+            result.push(arr[i])
+    }
+    }
+    return result;
+}
+console.log(bouncer([7, "ate", "", false, 9]));
