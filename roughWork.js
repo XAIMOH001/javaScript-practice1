89475f6fbcf0795ef9432c440f91c24184e82c46
XAIMOH001 <xaimohchau@gmail.com>
2025-11-01 20:51:36 +0300

    modified roughwork

--- roughWork.js
+++ roughWork.js
@@ -1,4 +1,4 @@
 //DOM MANIPULATION
 const heading = document.createElement('h1');
 heading.textContent = 'Hello, World!';
-document.body.appendChild(heading);     
+document.body.appendChild(heading);     S
