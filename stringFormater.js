e1c0c7e42e3d053b24a4e1afd2a8f62764e6e77d
XAIMOH001 <xaimohchau@gmail.com>
2026-01-26 22:04:54 +0300

    updated for camel casing strings

--- stringFormater.js
+++ stringFormater.js
@@ -7,11 +7,11 @@ console.log("Result of trimming whitespace from both ends:");
 console.log(cleanedInput);
 
 const trimmedStart = userInput.trimStart();
-console.log("After using the trimStart() method — leading spaces removed:");
+console.log("After using the trimStart() method, leading spaces removed:");
 console.log(trimmedStart);
 
 const trimmedEnd = userInput.trimEnd();
-console.log("After using the trimEnd() method — trailing spaces removed:");
+console.log("After using the trimEnd() method, trailing spaces removed:");
 console.log(trimmedEnd);
 
 const upperCaseInput = cleanedInput.toUpperCase();
@@ -23,7 +23,6 @@ console.log("Result of using the toLowerCase() method:");
 console.log(lowerCaseInput);
 
 const lowercaseWord = "camelcase";
-const camelCasedVersion = lowercaseWord.slice(0, 5);
-
+const camelCasedVersion = lowercaseWord.slice(0, 5) + lowercaseWord[5].toUpperCase() + lowercaseWord.slice(-3); 
 console.log("Camel cased version:");
 console.log(camelCasedVersion);
